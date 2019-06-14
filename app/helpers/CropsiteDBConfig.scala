d49c80d4ba68d5b6cac0b05bfcfa2f69b4ea7baa
MengZhang <meng.zhang15@ufl.edu>
2019-06-14 02:12:13 -0400

    change the default navi service to AgMIP server

--- app/helpers/CropsiteDBConfig.scala
+++ app/helpers/CropsiteDBConfig.scala
@@ -3,7 +3,7 @@ package cropsitedb.helpers
 import play.api.Play
 
 object CropsiteDBConfig {
-  val naviUrl = rewriteUrl(Play.current.configuration.getString("navi.baseurl").getOrElse("http://localhost:8082/navi/1"))
+  val naviUrl = rewriteUrl(Play.current.configuration.getString("navi.baseurl").getOrElse("https://api.agmip.org/navi/1"))
   val agtrialsUrl = rewriteUrl(Play.current.configuration.getString("extern.agtrials.url").getOrElse("http://localhost:8083/api/agtrials"))
   val localFileStore = rewriteUrl(Play.current.configuration.getString("datastore.local.directory").getOrElse("."))
 
